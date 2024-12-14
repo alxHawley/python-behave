3b3c6eb7d6f18a84606c696c4825329cf067d624
Alex Hawley <102092963+alxHawley@users.noreply.github.com>
2024-12-14 11:53:14 -0800

    update env
    * update env file to use env variable for UI tests
    * add ui variable to github actions workflow so that selenium is not set up for API tests

--- features/environment.py
+++ features/environment.py
@@ -2,17 +2,21 @@
 This module contains the Selenium env setup.
 """
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+import os
 
 
 def before_all(context):
-    """Setup chrome browser"""
-    service = Service("C:/Program Files/Google/Chrome/Application/chromedriver.exe")
-    context.browser = webdriver.Chrome(service=service)
-    context.browser.maximize_window()
+    """Setup chrome browser if running UI tests"""
+    if os.getenv("RUN_UI_TESTS") == "true":
+        from selenium import webdriver
+        from selenium.webdriver.chrome.service import Service
+
+        service = Service("C:/Program Files/Google/Chrome/Application/chromedriver.exe")
+        context.browser = webdriver.Chrome(service=service)
+        context.browser.maximize_window()
 
 
 def after_all(context):
-    """Close chrome browser"""
-    context.browser.quit()
+    """Close chrome browser if running UI tests"""
+    if os.getenv("RUN_UI_TESTS") == "true" and hasattr(context, "browser"):
+        context.browser.quit()
