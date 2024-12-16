61b02ff1f16348ea699483115688f21a5dc4e4c4
Alex Hawley <102092963+alxHawley@users.noreply.github.com>
2024-12-16 14:29:54 -0800

    Update environment.py
    * removed debug logging

--- features/environment.py
+++ features/environment.py
@@ -8,15 +8,10 @@ import logging
 def before_all(context):
     """Setup logging"""
     logging.basicConfig(level=logging.INFO)
-    logging.info("before_all: Logging setup complete")
 
 
 def before_scenario(context, scenario):
     """Setup chrome browser if running UI tests"""
-    logging.info(
-        f"before_scenario: Running scenario '{scenario.name}' "
-        f"with tags {scenario.effective_tags}'"
-    )
     if "ui" in scenario.effective_tags:
         from selenium import webdriver
         from selenium.webdriver.chrome.options import Options
@@ -43,15 +38,9 @@ def before_scenario(context, scenario):
             service=service, options=chrome_options
         )
         context.browser.maximize_window()
-        logging.info("before_scenario: Browser setup complete")
 
 
 def after_scenario(context, scenario):
     """Close chrome browser if running UI tests"""
-    logging.info(
-        f"after_scenario: Running scenario '{scenario.name}' "
-        f"with tags {scenario.effective_tags}"
-    )
     if "ui" in scenario.effective_tags and hasattr(context, "browser"):
         context.browser.quit()
-        logging.info("after_scenario: Browser closed")
